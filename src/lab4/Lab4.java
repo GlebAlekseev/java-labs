d8ddb3bca49c01d6d34259cf6c4963b0e7c4cf07
GlebAlekseev <glebalekseevjk@gmail.com>
2023-01-13 02:18:02 +0300

    lab 4

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -1,8 +1,31 @@
 package lab4;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
 public class Lab4 {
     public static void execute(){
+        String fileName = "rfc1.txt";
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            readFile(fileName, list);
+            Collections.sort(list);
+            for (int i = 0; i < list.size(); i++){
+                System.out.println("Line" + i + 1 +": \t" + list.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
 
+    private static void readFile(String fileName, Collection<String> collection) throws IOException {
+        Scanner scanner = new Scanner(new File(fileName), StandardCharsets.UTF_8);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            collection.add(line);
+        }
     }
 }
 
